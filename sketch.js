772baa2025be4d5675e10d644b8348fd0555b6d6
Pete Goodliffe <pgoodliffe@inmusicbrands.com>
2023-10-14 16:11:58 +0100

    Image loading is less tedious

--- sketch.js
+++ sketch.js
@@ -1,28 +1,10 @@
 // -------------------------------------------- ** declare variables ** -----------------------------------------------------
-let img0;
-let img1;
-let img2;
-let img3;
-let img4;
-let img5;
-let img6;
-let img7;
-let img8;
-let img9;
-let img10;
-let img11;
-let img12;
-let img13;
-let img14;
-let img15;
-//let img16;
 let randomImage = [];
-let randomVar;
 
 let numPennies = 15;
 let currentPenny = 0;
-let pennyX = [-1];
-let pennyY = [-1];
+let pennyX = [];
+let pennyY = [];
 
 let randomThought = [
   "My thought one",
@@ -37,43 +19,23 @@ let button;
 function preload() {
 
   // ---------------------------------------- ** loading images ** ----------------------------------------------------------
-  img0 = loadImage('pennies-01.png');
-  img1 = loadImage('pennies-02.png');
-  img2 = loadImage('pennies-03.png');
-  img3 = loadImage('pennies-04.png');
-  img4 = loadImage('pennies-05.png');
-  img5 = loadImage('pennies-06.png');
-  img6 = loadImage('pennies-07.png');
-  img7 = loadImage('pennies-08.png');
-  img8 = loadImage('pennies-09.png');
-  img9 = loadImage('pennies-10.png');
-  img10 = loadImage('pennies-11.png');
-  img11 = loadImage('pennies-12.png');
-  img12 = loadImage('pennies-13.png');
-  img13 = loadImage('pennies-14.png');
-  img14 = loadImage('pennies-15.png');
-  img15 = loadImage('pennies-16.png');
-  //img16 = loadImage('pennies-17.png');
-
-  //---------------------------------------------** set array of images ** ---------------------------------------------------
-
-  randomImage[0] = img0;
-  randomImage[1] = img1;
-  randomImage[2] = img2;
-  randomImage[3] = img3;
-  randomImage[4] = img4;
-  randomImage[5] = img5;
-  randomImage[6] = img6;
-  randomImage[7] = img7;
-  randomImage[8] = img8;
-  randomImage[9] = img9;
-  randomImage[10] = img10;
-  randomImage[11] = img11;
-  randomImage[12] = img12;
-  randomImage[13] = img13;
-  randomImage[14] = img14;
-  randomImage[15] = img15;
-  //randomImage[16] = img16;
+  randomImage[0] = loadImage('pennies-01.png');
+  randomImage[1] = loadImage('pennies-02.png');
+  randomImage[2] = loadImage('pennies-03.png');
+  randomImage[3] = loadImage('pennies-04.png');
+  randomImage[4] = loadImage('pennies-05.png');
+  randomImage[5] = loadImage('pennies-06.png');
+  randomImage[6] = loadImage('pennies-07.png');
+  randomImage[7] = loadImage('pennies-08.png');
+  randomImage[8] = loadImage('pennies-09.png');
+  randomImage[9] = loadImage('pennies-10.png');
+  randomImage[10] = loadImage('pennies-11.png');
+  randomImage[11] = loadImage('pennies-12.png');
+  randomImage[12] = loadImage('pennies-13.png');
+  randomImage[13] = loadImage('pennies-14.png');
+  randomImage[14] = loadImage('pennies-15.png');
+  randomImage[15] = loadImage('pennies-16.png');
+  randomImage[16] = loadImage('pennies-17.png');
 
 }
 
