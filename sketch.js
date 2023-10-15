1cb5ada7f89668610c569d3b0e23e0aad0bfba46
AGoodliffe <a.goodliffe0220201@arts.ac.uk>
2023-10-15 18:43:47 +0100

    adding thoughts, centring text and making italic

--- sketch.js
+++ sketch.js
@@ -6,11 +6,59 @@ let currentPenny = 0;
 let pennyX = [];
 let pennyY = [];
 
+// ---------------------------------------- ** array to store thoughts ** -------------------------------------------------
 let randomThought = [
-  "My thought one",
-  "My thought two",
-  "My thought three",
-  "My thought four"
+  "I\’m really hyper aware that my shoes smell really bad at the moment",
+  "can people see my get changed from my window",
+  "I've been attempting to work out what is god to me, and then deciding that I am pantheist",
+  "What's for dinner?",
+  "Will it get easier?",
+  "What needs doing?",
+  "The formalisation of Game Theory has been instrumental in our capability to delude ourselves for the sake of progress.",
+  "I cant decide if I like that it\’s autumn because the leaves are pretty and music sounds better or if I hate that it’s autumn because it gets dark early and my laundry doesn’t dry quickly",
+  "Am I at risk of the French bed bug epidemic?",
+  "Am I doing the right thing and how am I supposed to know",
+  "I really really hate our government. They all seem so incapable, and yet so cruel.",
+  "is my favourite colour actually blue or did I just decide that when I was younger and never thought about it since",
+  "fuck rishi sunak",
+  "Why can I never be bothered to leave the house anymore",
+  "Am I a good person",
+  "Does my boyfriend actually like me",
+  "Why is everyone around me so stupid",
+  "Time",
+  "If ppl didn’t see it did it really happen?",
+  "Do I have fleas",
+  "What was named first the orange fruit or colour",
+  "My football team is rubbish",
+  "Imagine if apps that sell clothes could let you virtually try them on from your camera in real time with every clothing item, even like e-Bay u know, life would be so good",
+  "should i bet against the team i support every game so that even if i lose, i win?",
+  "How my girlfriend doesn’t think about the Roman Empire as much as I do.",
+  "I only smoke abroad",
+  "My year abroad was a holiday",
+  "I need to buy some flowers",
+  "Talk, talk, talk",
+  "If could change the world what would I do first?",
+  "Kindness is always proritable",
+  "I’m going to fail uni",
+  "Being bored in a lecture is the most boring type of boring",
+  "Peepee poopoo",
+  "Take a day off :) or maybe a few",
+  "What is going through men’s heads?",
+  "I really want ice cream",
+  "We need more hand wash",
+  "Frogs",
+  "I really want some crisps from the bar",
+  "Cricket",
+  "I’ve broken the seal ffs",
+  "Cereal genuinely should be put in the bowl before milk, it shouldn’t even be a debate",
+  "Nostalgia is as sad as it is happy, very bittersweet",
+  "There's no difference between existence and nonexistence.",
+  "We're all just the universe experiencing itself. Maybe one day we'll realise that we never actually existed in the first place, and it makes no sense to ask why there's something rather than nothing.",
+  "Cupcakes are temporary but death is forever.",
+  "Do I have to get out of bed?",
+  "Not looking forward to today",
+  "Why does 3 pints make me hungover?"
+
 ];
 let currentThought = randomThought[0];
 
@@ -42,7 +90,6 @@ function preload() {
   randomImage[15] = loadImage('pennies-16.png');
   randomImage[16] = loadImage('pennies-17.png');
 
-  //myFont = loadFont('https://use.typekit.net/fhc7aff.css');
 
 }
 
@@ -66,7 +113,7 @@ function setup() {
   button.style('border-width', 0);
   //button.position();
   //button.position((windowWidth/2), (windowHeight/1.25));
-  button.position((windowWidth/2)-(button.width), (windowHeight/1.25));
+  button.position((windowWidth/2)-(button.width), (windowHeight/1.2));
   button.mousePressed(onButtonPressed);
   //button.setAutoDraw(false);
 }
@@ -94,13 +141,22 @@ function drawThought() {
 
   fill(81, 31, 18);
   textFont('bookmania');
+  textStyle('normal');
   textSize(windowWidth/30);
   text
   //text('p5*js', 10, 50);
   textAlign(CENTER);
   text("PENNY FOR YOUR THOUGHTS", (windowWidth/2), (windowHeight/10));
+  
+  
+  // ----------------------------------------- ** draw current though to screen ** ----------------------------------------
+  //text(currentThought, (windowWidth/2), (windowHeight/2));
+  textFont('bookmania', 'serif', 'italic');
+  textStyle('italic');
   textSize(windowWidth/50);
-  text(currentThought, (windowWidth/2), (windowHeight/2));
+  textWrap(WORD);
+  //text.center('horizontal');
+  text(currentThought, ((windowWidth/2)-350), (windowHeight/2.25), 700, 500);
 }
 
 function draw() {
